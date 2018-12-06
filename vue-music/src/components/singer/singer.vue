791f136ebffde0f80ca3415c03032b1d37af7095
ling <971039649@qq.com>
2018-12-06 14:43:52 +0800

    歌手页面制作

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -5,27 +5,59 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {getSingerList} from "api/singer"
-  import {ERR_OK} from "api/config";
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
 
   export default {
-    data(){
-      return{
-        singers:[]
+    data() {
+      return {
+        singers: []
       }
     },
-    created(){
-      this._getSingerList();
+    created() {
+      this._getSingerList()
     },
-    methods:{
-      _getSingerList(){
-        getSingerList().then((res)=>{
-          if(res.code === ERR_OK){
-            // this.singers = res.data;
-            console.log(res.data)
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this._normaliseSinger(this.singers))
           }
         })
       },
+      _normaliseSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: [],
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id:item.Fsinger_mid,
+            name:item.Fsinger_name
+          }))
+        })
+        console.log(map)
+      }
     }
   }
 </script>
